12a2f2c7cd0aa4c8db9425f33a7cac370928d925
jPedroRoch4 <jpedro.devops@gmail.com>
2023-04-05 21:33:49 -0300

    2023.04.05-2

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 $(document).ready(myApp)
 
-
 function myApp() {
 
-console.log("Funciona!!")
+    //Carrega a página inicial.
+    loadpage('home')
+
+}
+
+// Carrega uma página.
+function loadpage(page) {
+
+    //Monta os caminhos para os componemtes da página solicitar.
+    const path = {
+        html: `/pages/${page}/index.html`,
+        css: `/pages/${page}/index.css`,
+        js: `/pages/${page}/index.js`
+    }
+
+    $.get(path.html)
+        .done((data) => {
+            $('main').html(data)
+        })
+        .fail((error) => {
+            
+        })
 
 }
