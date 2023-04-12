5ae85ef0290471ba7d87640974227d1b2aa92008
jPedroRoch4 <jpedro.devops@gmail.com>
2023-04-11 21:45:52 -0300

    2023.04.11-3/Contatos quase finalizado

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,1 +1,35 @@
-console.log('Home funciona')
+$(document).ready(myContacts)
+
+/**
+ * Função principal da página "contacts".
+ **/
+function myContacts() {
+    /**
+     * Altera o título da página quando 'contacts' for acessada.
+     **/
+    changeTitle('Faça contato')
+
+    $(document).on('submit', '#cForm', sendContact)
+
+}
+
+function sendContact(ev) {
+
+    var formJSON = {}
+
+    const formData = new FormData(ev.target);
+
+    for (const [key, value] of formData) {
+        formJSON[key] = value
+    }
+
+    formJSON = formJSON.stringfy(formJSON)
+
+    console.log(formJSON)
+
+
+    return false
+
+
+
+}
